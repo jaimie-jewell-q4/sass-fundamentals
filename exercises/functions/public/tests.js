8a003cf6a32e8263cab8d4cb485397b4d7608eea
jaimie-jewell-q4 <59621360+jaimie-jewell-q4@users.noreply.github.com>
2021-03-01 13:24:13 -0500

    functions exercise

--- exercises/functions/public/tests.js
+++ exercises/functions/public/tests.js
@@ -1,6 +1,6 @@
-(function() {
+(function () {
   function testTheme(themename, c1, c2, c3, c4) {
-    test(themename + ' button colors', function(assert) {
+    test(themename + ' button colors', function (assert) {
       assert.hasStyle('.' + themename + ' .btn.btn-primary', {
         'background-color': c1
       }, 'primary button bg color is incorrect');
@@ -15,7 +15,7 @@
       }, 'other2 button bg color is incorrect');
     });
   }
-  test('First button', function(assert) {
+  test('First button', function (assert) {
     assert.hasStyle('.btn', {
       'border-radius': assert.compare.eq('2px'),
       'border-style': 'solid',
@@ -27,5 +27,5 @@
 
   testTheme('theme-1', '#339999', '#3c4d1a', '#3c1a4d', '#4d1a1a');
   testTheme('theme-2', '#660099', '#006644', '#664400', '#226600');
-  testTheme('theme-3', '#cc6699', '#336699', '#669933', '#339966');
+  testTheme('theme-3', '#cc6699', '#336699', '#336699', '#339966');
 }());
